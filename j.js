6c292276f17a06b09b8a7dfaef479ef6ea4c43d8
Jeet235 <jeetd1216@gmail.com>
2025-04-17 10:40:43 +0530

    Delete j.js

--- j.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-  $(".thumbnails img").click(function() {
-    var newSrc = $(this).attr("src").replace("150x100", "600x400");
-    $("#main").attr("src", newSrc);
-  });
-});
